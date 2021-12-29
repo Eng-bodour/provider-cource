00bb0fe8e5a842fc093a2cbe1c6a9bc54c03bcc1
bodour <git@github.com>
2021-12-29 22:26:20 +0300

    --6- Provider.of solve be use class contain provider.of so you inside changenotifiProvider

--- lib/providerof.dart
+++ lib/providerof.dart
@@ -11,42 +11,50 @@ class ProviderOf extends StatefulWidget {
 class _ProviderOfState extends State<ProviderOf> {
   @override
   Widget build(BuildContext context) {
-    var model = Provider.of<Model>(context);
+    //you show ERROR because use Provider.of again out domain ChangeNotifiProfider
+    // var model = Provider.of<Model>(context);
     return ChangeNotifierProvider(
       create: (context) => Model(),
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Provider-Courses-Scope'),
-          centerTitle: true,
-          backgroundColor: Colors.brown[300],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                model.text,
-                style: const TextStyle(
-                    color: Colors.brown,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              MaterialButton(
-                color: Colors.brown,
-                onPressed: () {
-                  model.changeText();
-                },
-                child: const Text(
-                  'Changed Text',
-                  style: TextStyle(color: Colors.white),
-                ),
-              )
-            ],
+          appBar: AppBar(
+            title: const Text('Provider-Courses-Scope'),
+            centerTitle: true,
+            backgroundColor: Colors.brown[300],
           ),
-        ),
+          body: const WidgetAll()),
+    );
+  }
+}
+
+class WidgetAll extends StatelessWidget {
+  const WidgetAll({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var model = Provider.of<Model>(context);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            model.text,
+            style: const TextStyle(
+                color: Colors.brown, fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          MaterialButton(
+            color: Colors.brown,
+            onPressed: () {
+              model.changeText();
+            },
+            child: const Text(
+              'Changed Text',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
       ),
     );
   }
