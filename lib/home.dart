45e1c2ec224146060bf918a7a002b22d6673ade1
bodour <git@github.com>
2021-12-29 18:15:30 +0300

    --2-  Change Notifier  Provider

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,44 +9,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String text = "Welcom";
+  // String text = "Welcom";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Provider-Courses'),
-        centerTitle: true,
-        backgroundColor: Colors.brown[300],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              text,
-              style: const TextStyle(
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            MaterialButton(
-              color: Colors.brown,
-              onPressed: () {
-                setState(() {
-                  text = "Bodour";
-                });
-              },
-              child: const Text(
-                'Changed Text',
-                style: TextStyle(color: Colors.white),
+    return ChangeNotifierProvider(
+      create: (context) => Model(),
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Provider-Courses'),
+            centerTitle: true,
+            backgroundColor: Colors.brown[300],
+          ),
+          body: Consumer<Model>(
+            builder: (context, valueModel, child) => Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    valueModel.text,
+                    style: const TextStyle(
+                        color: Colors.brown,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  MaterialButton(
+                    color: Colors.brown,
+                    onPressed: () {
+                      valueModel.changeText();
+                    },
+                    child: const Text(
+                      'Changed Text',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
+          )),
     );
+  }
+}
+
+class Model extends ChangeNotifier {
+  String text = "Hello";
+  void changeText() {
+    text = "Bodour";
+    notifyListeners();
   }
 }
